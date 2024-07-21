9951f32931a4349c66c9a86927b63088c9799a95
Faiz-fs <118742111+Faiz-fs@users.noreply.github.com>
2024-07-22 01:04:41 +0530

    Update settings.py

--- PointTask/settings.py
+++ PointTask/settings.py
@@ -18,7 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-API_URL="http://127.0.0.1:8000/api"
+
+
+API_URL="https://pointtask.onrender.com/api"
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +32,7 @@ SECRET_KEY = "django-insecure-j=)7raq$5b)=_p1pcdn#i*&8txbyw1k#+t!25+nflo4i78*-g!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -124,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (    
+    ('assets', os.path.join(PROJECT_DIR, '../static')),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
