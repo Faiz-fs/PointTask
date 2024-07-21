f71dce7ad5583b880e120a398f6d2fffffc1e8bf
Faiz-fs <118742111+Faiz-fs@users.noreply.github.com>
2024-07-22 01:05:54 +0530

    Update settings.py

--- PointTask/settings.py
+++ PointTask/settings.py
@@ -127,11 +127,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (    
-    ('assets', os.path.join(PROJECT_DIR, '../static')),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
